0419377397ec218bc4b4f886e88bfdb186739018
Unchaptered <workstation19961002@gmail.com>
2021-07-25 19:34:28 +0900

    07-25

--- js/input_todo.js
+++ js/input_todo.js
@@ -1,8 +1,7 @@
-const todo_form=documnet.querySelector("#nav_right");
+const todo_form=document.querySelector("#input_todo-form");
 const todo_input=document.querySelector("#input_todo");
 todo_form.addEventListener("submit",a);
 
 function a(prevent){
     prevent.preventDefault(); // submit 시 새로고침 방지
-    
 }
