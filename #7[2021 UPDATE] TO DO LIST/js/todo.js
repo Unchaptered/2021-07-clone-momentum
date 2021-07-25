115179397da1d4b709ae4a5e2c8825896f6f23e2
Unchaptered <workstation19961002@gmail.com>
2021-07-25 19:14:41 +0900

    07-25

--- #7[2021 UPDATE] TO DO LIST/js/todo.js
+++ #7[2021 UPDATE] TO DO LIST/js/todo.js
@@ -2,24 +2,30 @@ const toDoForm=document.getElementById("todo-form");
 const toDoInput=document.querySelector("#todo-form input");
 const toDoList=document.getElementById("todo-list");
 toDoForm.addEventListener("submit", handleToDoSubmit);
-const toDos=[];
+let toDos=[];
 const TODOS_KEY="todos";
 const savedToDos=localStorage.getItem(TODOS_KEY);
 
 function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo=toDoInput.value;
+    const newToDoObj={
+        text:newToDo,
+        id:Date.now(),
+    }
     toDoInput.value="";
-    toDos.push(newToDo);
-    paintToDo(newToDo);
+    toDos.push(newToDoObj);
+    paintToDo(newToDoObj);
     saveToDo();
 }
 
+// 화면에 그려주는 기능
 function paintToDo(newToDo){
     const li=document.createElement("li");
+    li.id=newToDo.id;
     const span=document.createElement("span");
     const button=document.createElement("button");
-    span.innerHTML=newToDo;
+    span.innerHTML=newToDo.text;
     button.innerHTML="❌";
     button.addEventListener("click", deleteToDo);
     
@@ -28,12 +34,17 @@ function paintToDo(newToDo){
     toDoList.appendChild(li); 
 }
 
+// 버튼을 누르면 해당 리스트를 지워주는 기능
 function  deleteToDo(event){
     event.preventDefault();
     const li=event.target.parentElement;
+    toDos=toDos.filter(toDo => toDo.id !== parseInt(li.id));
+    // 우리가 클릭한 toDo 의 id와 다른 것은 남겨두고 싶어!
     li.remove();
+    saveToDo();
 }
 
+// toDos 리스트를 localStorage 에 저장해주는 기능
 function saveToDo(event){
     localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
 }
@@ -41,6 +52,6 @@ function saveToDo(event){
 console.log(savedToDos);
 if(savedToDos !==null){
     const parseToDos=JSON.parse(savedToDos);
-    console.log(parseToDos);
-    parseToDos.forEach((item) => console.log("this is the turn of ", item));
+    toDos=parseToDos;
+    parseToDos.forEach(paintToDo);
 }
